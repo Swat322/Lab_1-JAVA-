cc8d8a476e0381bd18ec528da03dc0942742acda
Swat322 <nowallet21@ukr.net>
2019-03-01 16:27:54 +0200

    OP

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
+
 public class Main {
 	public static void main(String[] args){
-		Motor BMW = new Motor(4, 300 ,5 ,400,100,150);
+		Motor BMW = new Motor(4, 300 ,5 ,400,100,150 , 250, 275);
 		System.out.println(BMW.name = "Engine is Diesel");
 		System.out.println(BMW.high_voltage);
 		System.out.println(BMW.Power_consumption );
 		System.out.println(BMW.getPriceIndollars());
-		Motor Nissan = new Motor(4 , 300 , 5 ,400 , 100,150 );
+		System.out.println(BMW.getWeight());
+		Motor Nissan = new Motor(4 , 300 , 5 ,400 , 100,150 , 250, 275 );
 		System.out.println(Nissan.name1 = "Engine is Gasoline");
 		System.out.println(Nissan.high_voltage1);
 		System.out.println(Nissan.Power_consumption1);
 		System.out.println(Nissan.getPriceIndollars1());
-				
+		System.out.println(Nissan.getWeight1());
+		Nissan.resetValues("New name", 6, 500);
+		BMW.printnumberOfengine();
 	}
+
+}
--- Motor.java
+++ Motor.java
@@ -9,19 +9,37 @@ public class Motor {
 	public   int Power_consumption1 = 400;
 	private int  priceIndollars = 100 ;
 	private int  priceIndollars1 = 150;
+	private int weight = 250;
+	private int weight1 = 275;
 	
 	public Motor(int h , int P, int h1 , int P1){
 		this.high_voltage = h;
 		this.Power_consumption = P;
 		this.high_voltage1 = h1;
 		this.Power_consumption1  = P1;
+		numberOfengine ++;
 	}
-	public Motor (int high_voltage,int Power_consumption,int high_voltage1,int Power_consumption1,int p , int p1){
+	public Motor (int high_voltage,int Power_consumption,int high_voltage1,int Power_consumption1,int p , int p1 , int w ,int w1){
 		this( high_voltage,Power_consumption,high_voltage1,Power_consumption1);
 		setPriceIndollars(p);
 		setPriceIndollars1(p1);
+		setWeight(w);
+		setWeight1(w1);
 	}
-	
+	public static int getNumberofEngine() {
+	    return numberOfengine;
+	}
+
+	public static void setNumberofEngine(int numberOfengine) {
+	    Motor.numberOfengine = numberOfengine;
+	}
+	public static void printStaticnumberOfengine() {
+	    System.out.println("numberOfengine:" + numberOfengine);
+	}
+
+	public void printnumberOfengine() {
+	    System.out.println("numberOfengine:" + numberOfengine);
+	}
 	public int getPriceIndollars() {
 		return priceIndollars;
 	}
@@ -33,5 +51,26 @@ public class Motor {
 	}
 	public void setPriceIndollars1(int priceIndollars1) {
 		this.priceIndollars1 = priceIndollars1;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getWeight1() {
+		return weight1;
+	}
+	public void setWeight1(int weight1) {
+		this.weight1 = weight1;
+	}
+	public void resetValues(String name, int high_voltage, int Power_consumption) {
+	    this.name = name;
+	    this.high_voltage = high_voltage;
+	    this.Power_consumption = Power_consumption;
+	   
+	}
+	public String toString(){
+		return name + high_voltage + Power_consumption;
 	}
 }
